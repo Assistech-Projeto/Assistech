6e3ae66a78eee0b32feca117a27bcad51f964651
thecarfi <144296420+thecarfi@users.noreply.github.com>
2024-06-06 21:11:07 -0400

    henrique
    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>tela de inicio</p>
+    <ul>
+        <li><a href="#">cadastros de usuários</a></li>
+        <li><a href=""></a></li>
+    </ul>
+    <button><a href="https://www.freecodecamp.org/">cadastros de usuários</a></button>
+</body>
+</html>
